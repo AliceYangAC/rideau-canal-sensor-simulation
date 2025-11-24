b5c0d6872d181e52c51b50fd6fa96b1673716897
AliceYangAC <yang0447@algonquinlive.com>
2025-11-24 16:00:43 -0500

    copied the demo code and modified it for the canal metrics

--- sensor_simulation.py
+++ sensor_simulation.py
@@ -1,13 +1,26 @@
+import os
 import time
 import random
 from azure.iot.device import IoTHubDeviceClient, Message
+from dotenv import load_dotenv
 
-CONNECTION_STRING = "Your IoT Hub device connection string here"
+# Explicitly load the .env file from this folder
+BASE_DIR = os.path.dirname(__file__)
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
+
+# Ice Thickness (cm)
+# Surface Temperature (°C)
+# Snow Accumulation (cm)
+# External Temperature (°C)
+
+CONNECTION_STRING = os.getenv("CONNECTION_STRING")
 
 def get_telemetry():
     return {
-        "temperature": random.uniform(20.0, 40.0),
-        "humidity": random.uniform(30.0, 70.0)
+        "ice_thickness": random.uniform(5.0, 30.0),
+        "surface_temperature": random.uniform(-10.0, 0.0),
+        "snow_accumulation": random.uniform(0.0, 20.0),
+        "external_temperature": random.uniform(-20.0, 5.0)
     }
 
 def main():
