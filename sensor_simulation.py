5185f91450071de369bd85ff122a4a8e8314bd74
AliceYangAC <yang0447@algonquinlive.com>
2025-11-25 20:50:58 -0500

    updated some of the random values

--- sensor_simulation.py
+++ sensor_simulation.py
@@ -16,9 +16,9 @@ locations ={"Dow's Lake": DOWS_LAKE_CONN_STR, "Fifth Avenue": FIFTH_AVE_CONN_STR
 
 def get_telemetry(location):
     return {
-        "ice_thickness": random.uniform(5.0, 30.0),
-        "surface_temperature": random.uniform(-10.0, 0.0),
-        "snow_accumulation": random.uniform(0.0, 20.0),
+        "ice_thickness": random.uniform(1.0, 70.0),
+        "surface_temperature": random.uniform(-20.0, 0.0),
+        "snow_accumulation": random.uniform(0.0, 45.0),
         "external_temperature": random.uniform(-20.0, 5.0),
         "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
         "location": location
@@ -31,20 +31,21 @@ def get_telemetry(location):
 # Data Format: JSON with timestamp
 
 def main():
-    
     print("Sending telemetry of 3 devices to IoT Hub...")
+    
+    clients = {loc: IoTHubDeviceClient.create_from_connection_string(conn_str)
+               for loc, conn_str in locations.items()}
     try:
-        for location in locations.keys():
-            client = IoTHubDeviceClient.create_from_connection_string(locations[location])
-            telemetry = get_telemetry(location)
-            message = Message(str(telemetry))
-            client.send_message(message)
-            print(f"Sent message: {message}")
+        while True:
+            for loc, client in clients.items():
+                telemetry = get_telemetry(loc)
+                message = Message(str(telemetry))
+                client.send_message(message)
             time.sleep(10)
     except KeyboardInterrupt:
         print("Stopped sending messages.")
     finally:
-        client.disconnect()
-
+        for client in clients.values():
+            client.disconnect()
 if __name__ == "__main__":
     main()
